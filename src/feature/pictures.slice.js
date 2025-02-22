af0036db4b75045ea14416b0caf011e2f179d6fd
BernCell <bern.cell@hotmail.com>
2025-02-22 23:54:51 +0100

    3eme commit set+add+ edit Pictures

---
+++ src/feature/pictures.slice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const picturesSlice = createSlice({
+    name: "pictures",
+    initialState: {
+        pictures: null,
+    },
+    reducers: {
+        setPicturesData: (state, { payload }) => {
+            state.pictures = payload;
+        },
+
+        addPicture: (state, { payload }) => {
+            state.pictures.push(payload)
+        },
+
+        editPicture: (state, { payload }) => {
+            state.pictures = state.pictures.map((pic) => {
+                if (pic.id === payload[1]) {
+                    return {
+                        ...pic,
+                        artist: payload[0],
+                    };
+                } else { return pic; }
+            })
+        }
+    },
+
+});
+
+export const { setPicturesData, addPicture, editPicture } = picturesSlice.actions
+export default picturesSlice.reducer;
